71e125387beb4b8a291f74840644635df47a4191
yureehh <fabbri.juri@gmail.com>
2023-11-15 00:16:38 +0100

    Refactoring

--- test.py
+++ test.py
@@ -4,68 +4,81 @@ import base64
 import requests
 from pathlib import Path
 from dotenv import load_dotenv
-
-# Constants
-API_URL = "https://api.openai.com/v1/chat/completions"
-MODEL = "gpt-4-vision-preview"
-MAX_TOKENS = 1024
-TEMPERATURE = 0
+from typing import Dict
 
 # Load environment variables
 load_dotenv()
 API_KEY = os.getenv("OPENAI_API_KEY")
+API_URL = os.getenv("API_URL", "https://api.openai.com/v1/chat/completions")
+MODEL = os.getenv("MODEL", "gpt-4-vision-preview")
+MAX_TOKENS = int(os.getenv("MAX_TOKENS", 1024))
+TEMPERATURE = int(os.getenv("TEMPERATURE", 0))
 
 if not API_KEY:
     raise EnvironmentError("Missing OPENAI_API_KEY environment variable")
 
-def load_json_config(file_path):
+def load_json_config(file_path: Path) -> Dict:
+    """Loads and returns JSON configuration from the given file path."""
     try:
-        with open(file_path, "r") as f:
-            return json.load(f)
-    except FileNotFoundError:
-        raise FileNotFoundError(f"Configuration file not found: {file_path}")
+        with file_path.open("r") as file:
+            return json.load(file)
+    except FileNotFoundError as e:
+        print(f"Configuration file not found: {file_path}")
+        raise e
 
-def encode_image_to_base64(image_path):
+def encode_image_to_base64(image_path: Path) -> str:
+    """Encodes the image at the given path to base64."""
     try:
-        with open(image_path, "rb") as image_file:
+        with image_path.open("rb") as image_file:
             return base64.b64encode(image_file.read()).decode('utf-8')
-    except FileNotFoundError:
-        raise FileNotFoundError(f"Image file not found: {image_path}")
+    except FileNotFoundError as e:
+        print(f"Image file not found: {image_path}")
+        raise e
 
-def build_payload(base64_image, prompt, double_check):
+def build_payload(base64_image: str, prompt: str, addendum: str) -> Dict:
+    """Builds the payload for the API request."""
     return {
         "model": MODEL,
         "messages": [
             {"role": "system", "content": [{"type": "text", "text": prompt}]},
             {"role": "user", "content": [{"type": "image_url", "image_url": f"data:image/jpeg;base64,{base64_image}"}]},
-            {"role": "system", "content": [{"type": "text", "text": double_check}]}
+            {"role": "user", "content": [{"type": "text", "text": addendum}]}
         ],
         "max_tokens": MAX_TOKENS
     }
 
-def classify_document(image_path, class_config):
-    base64_image = encode_image_to_base64(image_path)
-    prompt = class_config["prompt"]
-    double_check = class_config["double_check"]
-    payload = build_payload(base64_image, prompt, double_check)
+def post_request_to_api(payload: Dict) -> Dict:
+    """Posts a request to the API and returns the response."""
     headers = {"Content-Type": "application/json", "Authorization": f"Bearer {API_KEY}"}
-
-    response = requests.post(API_URL, headers=headers, json=payload)
-    if response.status_code == 200:
+    try:
+        response = requests.post(API_URL, headers=headers, json=payload)
+        response.raise_for_status()
         return response.json()["choices"][0]["message"]["content"]
-    else:
-        raise ConnectionError(f"API Request Failed: Status Code {response.status_code}, Response {response.text}")
+    except requests.RequestException as e:
+        print(f"API Request Failed: {e}")
+        raise
+
+def process_image(image_path: Path, config: Dict, task_type: str) -> str:
+    """Processes the image for a specified task."""
+    base64_image = encode_image_to_base64(image_path)
+    prompt = config.get(task_type, {}).get("prompt", "")
+    addendum = config.get(task_type, {}).get("addendum", "")
+    payload = build_payload(base64_image, prompt, addendum)
+    return post_request_to_api(payload)
 
 if __name__ == "__main__":
-    config_path = os.path.join("config", "classification.json")
-    config = load_json_config(config_path)
-    language = "it"
-    class_config = config["classification_prompts"][language]
-    document_path = os.path.join("docs", "01 - Dichiarazione di Conformità Lotto L 254SC selez_page1.png")
-
     try:
-        classification = classify_document(document_path, class_config)
-        print(classification)
+        config_path = Path("config/conf.json")
+        config = load_json_config(config_path)
+        language = "it"
+        class_config = config["classification_prompts"][language]
+        metadata_config = config["metadata_prompts"][language]
+        document_path = Path("docs/02 - Dichiarazione di Conformità Lotto L 255SC selez.png")
+
+        classification = process_image(document_path, class_config, "classification")
+        print(f"Classification: {classification}")
+        metadata = process_image(document_path, metadata_config, classification)
+        print(f"Metadata: {metadata}")
 
     except Exception as e:
         print(f"An error occurred: {e}")
