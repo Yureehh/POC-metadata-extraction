f207648b37e8411aaec4ae092bd3c80b5e0c55af
yureehh <fabbri.juri@gmail.com>
2024-01-16 23:46:48 +0100

    Refactored code

--- app.py
+++ app.py
@@ -1,211 +1,90 @@
-import base64
-import json
-import os
 from pathlib import Path
-from typing import Dict, Optional
 
 import gradio as gr
-import requests
-from dotenv import load_dotenv
 
-# Load environment variables
-load_dotenv()
-API_KEY = os.getenv("OPENAI_API_KEY")
-API_URL = "https://api.openai.com/v1/chat/completions"
-MODEL = os.getenv("MODEL", "gpt-4-vision-preview")
-MODEL2 = os.getenv("MODEL2", "gpt-4-1106-preview")
-MAX_TOKENS = int(os.getenv("MAX_TOKENS", 1024))
-TEMPERATURE = int(os.getenv("TEMPERATURE", 0))
-METADATA_OPTIONS = [
-    "IdLotto",
-    "IdLottoMadre",
-    "CodiceMateriaPrima",
-    "DDT",
-    "DataDDT",
-    "DataConsegna",
-    "PresenzaConformita'",
-]
+# Make sure all necessary functions are imported from utils
+from utils import (create_chatbot_payload, encode_image_to_base64_string,
+                   get_environment_settings, load_json_configuration,
+                   send_request_to_openai_api)
 
-# Verify if API key is available
-if not API_KEY:
-    raise EnvironmentError("Missing OPENAI_API_KEY environment variable")
+# Load environment variables and configuration
+env_vars = get_environment_settings()
+config = load_json_configuration()
 
 
-def load_json_config(file_path: Path) -> Dict:
-    """Loads and returns JSON configuration from the given file path."""
-    try:
-        with file_path.open("r") as file:
-            return json.load(file)
-    except FileNotFoundError as e:
-        print(f"Configuration file not found: {file_path}")
-        raise e
-
-
-def encode_image_to_base64(image_path: Path) -> Optional[str]:
-    """Encodes the image at the given path to base64."""
-    try:
-        with image_path.open("rb") as image_file:
-            return base64.b64encode(image_file.read()).decode("utf-8")
-    except FileNotFoundError as e:
-        print(f"Image file not found: {image_path}")
-        return None
-
-
-def build_payload(
-    base64_image: str, prompt: str, addendum: str, output: str, model: str = MODEL
-) -> Dict:
-    """Builds the payload for the API request."""
-    return {
-        "model": model,
-        "messages": [
-            {"role": "system", "content": [{"type": "text", "text": prompt}]},
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "image_url",
-                        "image_url": f"data:image/jpeg;base64,{base64_image}",
-                    }
-                ],
-            },
-            {"role": "user", "content": [{"type": "text", "text": addendum}]},
-            {"role": "user", "content": [{"type": "text", "text": output}]},
-        ],
-        "max_tokens": MAX_TOKENS,
-    }
-
-
-def build_payload2(metadata: str, prompt: str, model: str = MODEL2) -> Dict:
-    """Builds the payload for the API request. No image is passed."""
-    return {
-        "model": model,
-        "messages": [
-            {"role": "system", "content": [{"type": "text", "text": prompt}]},
-            {"role": "user", "content": [{"type": "text", "text": metadata}]},
-        ],
-        "max_tokens": MAX_TOKENS,
-        "temperature": TEMPERATURE,
-    }
-
-
-def post_request_to_api(payload: Dict) -> Dict:
-    """Posts a request to the API and returns the response."""
-    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {API_KEY}"}
-    try:
-        response = requests.post(API_URL, headers=headers, json=payload)
-        response.raise_for_status()
-        return response.json()["choices"][0]["message"]["content"]
-    except requests.RequestException as e:
-        print(f"API Request Failed: {e}")
-        raise
-
-
-def process_image(image_path: Path, config: Dict, task_type: str, **kwargs) -> str:
-    """Processes the image for a specified task."""
-    base64_image = encode_image_to_base64(image_path)
-    if base64_image is None:
+def process_image(
+    image_path: Path, config_section: dict, task_key: str, model: str
+) -> str:
+    """Process the image for a specified task using the OpenAI API."""
+    image_base64 = encode_image_to_base64_string(image_path)
+    if image_base64 is None:
         return "Error: Image encoding failed"
 
-    prompt = config.get(task_type, {}).get("prompt", "")
-    addendum = config.get(task_type, {}).get("addendum", "")
-    output = config.get(task_type, {}).get("output", "")
-    metadata_extraction_string = config.get("metadata_extraction_string", {})
+    payload = create_chatbot_payload(
+        image_base64,
+        config_section.get("prompt", ""),
+        config_section.get("addendum", ""),
+        config_section.get("output", ""),
+        model,
+        env_vars["max_tokens"],
+    )
+    return send_request_to_openai_api(payload, env_vars["api_key"], env_vars["api_url"])
 
-    if metadata_to_extract := kwargs.get("metadata_to_extract"):
-        prompt_addition = "".join(
-            f"{metadata}: [{metadata_extraction_string[metadata]}]\n"
-            for metadata in metadata_to_extract
+
+def main(
+    language: str, document_path: str, metadata_to_extract: list, selected_model: str
+) -> tuple:
+    """Main function to process the document image and extract required information."""
+    document_path = Path(document_path)
+    language_config = config[language]
+
+    classification = process_image(
+        document_path,
+        language_config["classification_prompts"],
+        "classification",
+        selected_model,
+    )
+    metadata = process_image(
+        document_path,
+        language_config["metadata_prompts"][classification],
+        classification,
+        selected_model,
+    )
+    tests = (
+        process_image(
+            document_path, language_config["tests_prompts"], "tests", selected_model
         )
-        prompt += f"{prompt_addition}."
+        if classification in ["COA", "SCD+COA"]
+        else "No tests to extract"
+    )
 
-    payload = build_payload(base64_image, prompt, addendum, output)
-    return post_request_to_api(payload)
-
-
-def process_metadata_with_llm(config: Dict, metadata: str, model: str = MODEL2) -> Dict:
-    """Processes the metadata and returns a dictionary."""
-    prompt = config.get("metadata", {}).get("prompt", "")
-    payload = build_payload2(metadata, prompt, model=model)
-    return post_request_to_api(payload)
-
-
-def process_metadata(metadata: str) -> Optional[Dict]:
-    """Attempts to parse JSON metadata and returns a dictionary if successful."""
-    try:
-        return "\n".join(
-            f"{key}: {value}" for key, value in json.loads(metadata).items()
-        )
-    except json.JSONDecodeError as e:
-        print(f"Error parsing JSON: {e}")
-        return "An error occurred while parsing the metadata"
-
-
-def main(language: str, document_path: str, metadata_to_extract: str) -> str:
-    try:
-        document_path = Path(document_path)
-        config_path = Path("config/conf.json")
-        config = load_json_config(config_path)
-        class_config = config["classification_prompts"][language]
-        metadata_config = config["metadata_prompts"][language]
-        tests_config = config["tests_prompts"][language]
-
-        classification = process_image(document_path, class_config, "classification")
-        metadata = process_image(
-            document_path,
-            metadata_config,
-            classification,
-            metadata_to_extract=metadata_to_extract,
-        )
-        parsed_metadata = process_metadata(metadata)
-        if classification in ["COA", "SCD+COA"]:
-            tests = process_image(document_path, tests_config, "tests")
-            tests = process_metadata(tests)
-        else:
-            tests = "No tests to extract"
-
-        return classification, parsed_metadata, tests
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None, None, None
+    return classification, metadata, tests
 
 
 if __name__ == "__main__":
     with gr.Blocks() as demo:
         with gr.Row():
-            document_path = gr.Image(type="filepath", label="Image")
-            with gr.Column():
-                model = gr.Dropdown(
-                    [MODEL], value=MODEL, label="Model", info="Which model to use?"
-                )
-                language = gr.Dropdown(
-                    ["it", "en"],
-                    label="Language",
-                    info="Which language is the document in?",
-                )
-                metadata_to_extract = gr.CheckboxGroup(
-                    METADATA_OPTIONS,
-                    value=METADATA_OPTIONS,
-                    label="Metadata",
-                    info="What metadata to extract?",
-                )
-        with gr.Row():
-            classification = gr.Textbox(
-                label="Classification", info="The document has been classified as:"
+            document_input = gr.Image(type="filepath", label="Upload Document Image")
+            model_selector = gr.Dropdown(list(env_vars["models"].keys()), label="Model")
+            language_selector = gr.Dropdown(["it", "en"], label="Document Language")
+            metadata_to_extract = gr.CheckboxGroup(
+                config["metadata_to_extract"], label="Metadata to Extract"
             )
-            metadata_extracted = gr.Textbox(
-                label="Metadata", info="The following metadata have been extracted:"
-            )
-            test_extracted = gr.Textbox(
-                label="Tests",
-                info="The following metadata have been extracted:",
-                max_lines = 100
-            )
-        query_btn = gr.Button("Ask")
-        query_btn.click(
-            fn=main,
-            inputs=[language, document_path, metadata_to_extract],
-            outputs=[classification, metadata_extracted, test_extracted],
-        )
 
-    demo.launch(share=True)
+        with gr.Row():
+            classification_display = gr.Textbox(label="Classification")
+            metadata_display = gr.Textbox(label="Extracted Metadata")
+            tests_display = gr.Textbox(label="Extracted Tests", max_lines=10)
+
+        analyze_button = gr.Button("Analyze")
+        analyze_button.click(
+            fn=main,
+            inputs=[
+                language_selector,
+                document_input,
+                metadata_to_extract,
+                model_selector,
+            ],
+            outputs=[classification_display, metadata_display, tests_display],
+        )
+        demo.launch(share=True)
---
+++ utils.py
@@ -0,0 +1,94 @@
+import base64
+import json
+import os
+from pathlib import Path
+from typing import Dict, Optional
+
+import requests
+
+
+def get_environment_settings():
+    if api_key := os.getenv("OPENAI_API_KEY"):
+        return {
+            "api_key": api_key,
+            "api_url": "https://api.openai.com/v1/chat/completions",
+            "models": {
+                "gpt-4-vision": os.getenv("MODEL", "gpt-4-vision-preview"),
+                "gpt-4-1106": os.getenv("MODEL2", "gpt-4-1106-preview"),
+            },
+            "max_tokens": int(os.getenv("MAX_TOKENS", 1024)),
+            "temperature": float(os.getenv("TEMPERATURE", 0.0)),  # Changed to float
+            "default_model": "gpt-4-vision",
+        }
+    else:
+        raise EnvironmentError("Missing OPENAI_API_KEY environment variable")
+
+
+def load_json_configuration():
+    config_path = Path("config/config.json")
+    if not config_path.exists():
+        raise FileNotFoundError("Configuration file not found at 'config/config.json'")
+    with config_path.open("r") as file:
+        return json.load(file)
+
+
+def encode_image_to_base64_string(image_path: Path) -> Optional[str]:
+    try:
+        with image_path.open("rb") as image_file:
+            return base64.b64encode(image_file.read()).decode("utf-8")
+    except FileNotFoundError:
+        print(f"Image file not found: {image_path}")
+        return None
+
+
+def create_chatbot_payload(
+    image_base64: str,
+    prompt: str,
+    addendum: str,
+    output: str,
+    model: str,
+    max_tokens: int,
+) -> Dict:
+    return {
+        "model": model,
+        "messages": [
+            {"role": "system", "content": [{"type": "text", "text": prompt}]},
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": f"data:image/jpeg;base64,{image_base64}",
+                    }
+                ],
+            },
+            {"role": "user", "content": [{"type": "text", "text": addendum}]},
+            {"role": "user", "content": [{"type": "text", "text": output}]},
+        ],
+        "max_tokens": max_tokens,
+    }
+
+
+def create_text_payload(
+    text: str, prompt: str, model: str, max_tokens: int, temperature: int
+) -> Dict:
+    return {
+        "model": model,
+        "messages": [
+            {"role": "system", "content": [{"type": "text", "text": prompt}]},
+            {"role": "user", "content": [{"type": "text", "text": text}]},
+        ],
+        "max_tokens": max_tokens,
+        "temperature": temperature,
+    }
+
+
+def send_request_to_openai_api(payload: Dict, api_key: str, api_url: str) -> Dict:
+    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {api_key}"}
+    try:
+        response = requests.post(api_url, headers=headers, json=payload)
+        response.raise_for_status()
+        return response.json()["choices"][0]["message"]["content"]
+    except requests.RequestException as e:
+        print(f"API Request Failed: {e}")
+        raise
